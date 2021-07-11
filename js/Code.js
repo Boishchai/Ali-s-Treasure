155832fedd7a37b97ce735764192ce8d384e8733
CoderAKS07 <79311937+CoderAKS07@users.noreply.github.com>
2021-07-11 18:54:25 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    text("A T A D A S B E", 700,150);
+    text("C U T N I F O N", 100,350);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+    // add code for displaying rest of the names and the hints.
+    text("Hint: Realtime Server Storage Unit !!", 700,170);
+    text("Hint: Requiring an argument, performing some task !!", 100,370);
+
+}
